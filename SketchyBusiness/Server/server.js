480bd2726fc7bb287c72adeeb537d908e950d9d9
WarriorSFG <samarthgupta9999@gmail.com>
2025-05-30 14:24:15 +0530

    Update server.js

--- SketchyBusiness/Server/server.js
+++ SketchyBusiness/Server/server.js
@@ -4,16 +4,17 @@ const path = require('path')
 const cors = require('cors')
 require('dotenv').config()
 
+const app = express()
+app.use(express.json())
+app.use(express.static(__dirname))
+app.use(express.urlencoded({ extended: true }))
+
 app.use(cors({
   origin: 'https://sketchy-business-frontend.vercel.app'
 }));
 
 const port = process.env.PORT
 const URL = process.env.MONGOURL
-const app = express()
-app.use(express.json())
-app.use(express.static(__dirname))
-app.use(express.urlencoded({ extended: true }))
 
 mongoose.connect(URL)
 
