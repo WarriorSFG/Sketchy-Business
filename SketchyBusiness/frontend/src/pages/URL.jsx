6f27efd17ff4e8a032a0f51330e669574e487297
WarriorSFG <samarthgupta9999@gmail.com>
2025-06-05 00:57:50 +0530

    Update URL.jsx

--- SketchyBusiness/frontend/src/pages/URL.jsx
+++ SketchyBusiness/frontend/src/pages/URL.jsx
@@ -1,3 +1,3 @@
-const BackendURL = "https://sketchy-business-backend.vercel.app/api";
+const BackendURL = "https://sketchy-business-backend.vercel.app";
 //const BackendURL = "http://localhost:4000"
 export default BackendURL;
